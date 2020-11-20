76c56ed4679bf8c9c64d65e8ace228c8c5169486
黄幸科 <huangxingke@lkimt.com>
2020-11-20 10:12:36 +0800

    选择导航模块逻辑优化

--- app/src/main/java/com/fitgreat/airfacerobot/chosedestination/ChoseDestinationActivity.java
+++ app/src/main/java/com/fitgreat/airfacerobot/chosedestination/ChoseDestinationActivity.java
@@ -1,48 +1,29 @@
 package com.fitgreat.airfacerobot.chosedestination;
 
-import android.net.Uri;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.alibaba.fastjson.JSON;
 import com.bumptech.glide.Glide;
 import com.fitgreat.airfacerobot.MyApp;
 import com.fitgreat.airfacerobot.R;
-import com.fitgreat.airfacerobot.automission.adapter.OperationAdapter;
 import com.fitgreat.airfacerobot.base.MvpBaseActivity;
 import com.fitgreat.airfacerobot.chosedestination.presenter.ChoseDestinationPresenter;
 import com.fitgreat.airfacerobot.chosedestination.view.ChoseDestinationView;
-import com.fitgreat.airfacerobot.model.ActionEvent;
 import com.fitgreat.airfacerobot.model.InitEvent;
 import com.fitgreat.airfacerobot.model.LocationEntity;
 import com.fitgreat.airfacerobot.launcher.utils.LocalCashUtils;
-import com.fitgreat.airfacerobot.launcher.widget.MyDialog;
 import com.fitgreat.airfacerobot.launcher.widget.YesOrNoDialogFragment;
 import com.fitgreat.airfacerobot.model.MapEntity;
 import com.fitgreat.archmvp.base.util.LogUtils;
 import com.fitgreat.archmvp.base.util.SpUtils;
-
 import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
-
 import java.util.List;
-
 import butterknife.BindView;
-import butterknife.OnClick;
-
 import static com.fitgreat.airfacerobot.constants.RobotConfig.AUTOMATIC_RECHARGE_TAG;
 import static com.fitgreat.airfacerobot.constants.RobotConfig.MAP_INFO_CASH;
 import static com.fitgreat.airfacerobot.constants.RobotConfig.MSG_CHANGE_FLOATING_BALL;
-import static com.fitgreat.airfacerobot.constants.RobotConfig.NAVIGATION_SUCCESS_TIP_TAG;
 
 /**
  * 选择我要去目的地页面
@@ -91,22 +72,20 @@ public class ChoseDestinationActivity extends MvpBaseActivity<ChoseDestinationVi
         Glide.with(this).load(mapEntity.getF_MapFileUrl()).into(mChoseDestinationMapBack);
         //添加导航点按钮位置
         for (int i = 0; i < locationList.size(); i++) {
-            locationList.get(i);
+            LocationEntity locationEntity = locationList.get(i);
             ImageButton imageButton = new ImageButton(this);
             imageButton.setBackgroundColor(getResources().getColor(R.color.transparent));
             imageButton.setImageDrawable(getResources().getDrawable(R.mipmap.ic_map_location_nor));
-            imageButton.setId(i);
             //单点位置按钮添加点击事件
-            int finalI = i;
             imageButton.setOnClickListener(v -> {
-                showDialogNavigation(true, "你现在要去\t" + locationList.get(finalI).getF_Name() + "\t吗?\t点击\"是\"我会带你过去哦", "是", "否", finalI);
+                showDialogNavigation(true, "你现在要去\t" + locationEntity.getF_Name() + "\t吗?\t点击\"是\"我会带你过去哦", "是", "否", locationEntity);
                 //自动回充工作流结束
                 SpUtils.putBoolean(MyApp.getContext(), AUTOMATIC_RECHARGE_TAG, false);
             });
             //设置导航点按钮位置
             RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.topMargin = locationCurrentY[i] - 85;
-            layoutParams.leftMargin = locationCurrentX[i] - 30;
+            layoutParams.topMargin = Integer.parseInt(locationEntity.getS_Y()) - 85;
+            layoutParams.leftMargin = Integer.parseInt(locationEntity.getS_X()) - 30;
             mChoseDestinationContainer.addView(imageButton, layoutParams);
         }
     }
@@ -114,14 +93,14 @@ public class ChoseDestinationActivity extends MvpBaseActivity<ChoseDestinationVi
     /**
      * 启动单点导航任务
      */
-    public void showDialogNavigation(boolean isStartNavigation, String dialogContent, String yesBtText, String noBtText, int locationPosition) {
+    public void showDialogNavigation(boolean isStartNavigation, String dialogContent, String yesBtText, String noBtText, LocationEntity locationEntity) {
         YesOrNoDialogFragment yesOrNoDialogFragment = YesOrNoDialogFragment.newInstance("提示", dialogContent, yesBtText, noBtText);
         yesOrNoDialogFragment.show(getSupportFragmentManager(), "navigation");
         yesOrNoDialogFragment.setSelectYesNoListener(new YesOrNoDialogFragment.SelectYesNoListener() {
             @Override
             public void selectYes() {
                 if (isStartNavigation) {
-                    mPresenter.startLocationTask(locationList.get(locationPosition));
+                    mPresenter.startLocationTask(locationEntity);
                 }
             }
 
